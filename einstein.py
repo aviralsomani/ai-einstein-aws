53359f06207a46a9333c853c42174a89d0db8164
Aviral Somani <avisomani@Avirals-MBP.attlocal.net>
2020-05-08 16:05:36 -0500

    First try of gpt2 run

---
+++ einstein.py
@@ -0,0 +1,19 @@
+get_ipython().system('pip install gpt_2_simple')
+
+import gpt_2_simple as gpt2
+import os
+
+seed = "hello"
+sess = gpt2.start_tf_sess()
+
+gpt2.load_gpt2(sess)
+text = gpt2.generate(sess,
+              length=500,
+              prefix=seed,
+              return_as_list=True)[0]
+
+print(text)
+
+
+
+
