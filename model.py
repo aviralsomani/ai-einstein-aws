f9a495e5b49c6fb7988d29055bc04b2333c95a10
Aviral Somani <avisomani@Avirals-MBP.attlocal.net>
2020-05-09 14:08:37 -0500

    fixed model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import gpt_2_simple as gpt2
 class EinsteinModel:
     def __init__(self):
         self.sess = gpt2.start_tf_sess()
-        gpt.load_gpt2(self.sess)
+        gpt2.load_gpt2(self.sess)
 
     def generate(self, prompt='Hello', length=500):
         text = gpt2.generate(self.sess, length=length, prefix=prompt, return_as_list=True)[0]
